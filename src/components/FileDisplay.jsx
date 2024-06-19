712fc76064e115ecc19d31e6c9dcb79baddbd35c
veronosmani <veron.osmani@gmail.com>
2024-06-19 13:27:00 +0200

    modified:   src/components/FileDisplay.jsx 	modified:   src/components/Header.jsx 	new file:   src/components/Information.jsx 	new file:   src/components/Transcribing.jsx

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function FileDisplay(props) {
   const { handleAudioReset, file, audioStream } = props;
   return (
-    <main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20 w-fit max-w-full mx-auto">
+    <main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20 sm:w-96 w-72 max-w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Your<span className="text-blue-400 bold">File</span>
       </h1>
-      <div className="mx-auto flex flex-col text-left my-4">
+      <div className="flex flex-col text-left my-4">
         <h3 className="font-semibold">Name</h3>
-        <p>{file.name}</p>
+        <p>{file ? file?.name : 'Costum audio'}</p>
       </div>
       <div className="flex items-center justify-between gap-4">
         <button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <h1 className="font-medium">
         Free<span className="text-blue-400 bold">Scribe</span>
       </h1>
-      <button className="flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400">
+      <button className="flex items-center gap-2 specialBtn px-3 text-sm py-2 rounded-lg text-blue-400">
         <p>New</p>
         <i class="fa-solid fa-plus"></i>
       </button>
